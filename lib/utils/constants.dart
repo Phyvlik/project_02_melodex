d1ac989ba0cd0476d2404c2b2948be908a5fff75
Edward Forrester <edwardforrester4@gmail.com>
2026-05-03 13:21:03 -0400

    Fix spotify integration

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -27,7 +27,7 @@ class AppConstants {
   static const double weightListenHistory = 0.2;
 
   //logging in to user spotify account
-  static const String spotifyClientId = 'YOUR_CLIENT_ID_HERE';
+  static const String spotifyClientId = 'bd268cd3fb5c4152899fe59dacd0533b';
 static const String spotifyRedirectUri = 'melodex://spotify-auth';
 
 static const String prefSpotifyRefreshToken = 'spotify_refresh_token';
